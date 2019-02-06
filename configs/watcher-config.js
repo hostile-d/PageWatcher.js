051b865d126b8a2844419535da95b845f6fa4e2c
hostile_d <host.chel@gmail.com>
2019-02-06 23:56:49 +0500

    change time

--- configs/watcher-config.js
+++ configs/watcher-config.js
@@ -1,5 +1,5 @@
-const SUCCESS_UPDATE_INTERVAL = 300000;
-const FAIL_UPDATE_INTERVAL = 3600000;
+const SUCCESS_UPDATE_INTERVAL = 180000;
+const FAIL_UPDATE_INTERVAL = 1200000;
 const SUCCESS_TEXT = encodeURIComponent('✅ There are some tickets 🎫');
 const FAIL_TEXT = encodeURIComponent('#UATMode 🚫 There are no tickets');
 
